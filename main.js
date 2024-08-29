868dae3954a08950196500525a40917c159a4690
Kubuntu Gonzalo <gonzad2001@hotmail.com>
2024-08-29 05:19:51 -0300

    add csv parse

--- main.js
+++ main.js
@@ -1,19 +1,63 @@
-import { helloWorld } from "./js/plot.js";
-import * as Plotly from "plotly.js-dist";
+import { parse } from "csv-parse/browser/esm/sync";
 
-let TESTER = document.getElementById("tester");
+const dataStore = {
+    raw_records: [],
+};
 
-Plotly.newPlot(
-    TESTER,
-    [
-        {
-            x: [1, 2, 3, 4, 5],
-            y: [1, 2, 4, 8, 16],
-        },
-    ],
-    {
-        margin: { t: 0 },
+const tableLookUp = {};
+
+document
+    .getElementById("csvFileInput")
+    .addEventListener("change", function (event) {
+        const file = event.target.files[0]; // Get the selected file
+        if (file) {
+            // TODO: add multiple csv functionality
+            // TODO: Wipe dataStore
+
+            const reader = new FileReader();
+
+            // When the file is loaded, read it as text
+            reader.onload = function (e) {
+                const csvContent = e.target.result;
+
+                // parse CSV
+                dataStore.raw_records = parse(csvContent, {
+                    columns: true,
+                    skip_empty_lines: true,
+                });
+
+                console.log(dataStore.raw_records); // CONSOLE_LOG
+
+                // on success create table look up
+                createTableLookUp();
+                console.log(tableLookUp);
+            };
+
+            // TODO: add ui feedback for error
+            reader.onerror = function () {
+                // TODO: handle error?
+                console.error("Error reading the file");
+            };
+
+            // Read the file as text
+            reader.readAsText(file);
+        }
+    });
+
+function createTableLookUp() {
+    let n = dataStore.raw_records.length;
+    if (n === 0) {
+        return;
     }
-);
 
-helloWorld();
+    for (let row in dataStore.raw_records[0]) {
+        tableLookUp[row] = [];
+
+        for (let i = 0; i < n; i++) {
+            let record_i = dataStore.raw_records[i];
+            tableLookUp[row].push([record_i[row], i]);
+        }
+
+        tableLookUp[row].sort();
+    }
+}
--- test/test.js
+++ test/test.js
@@ -1,16 +1,17 @@
 import { assert } from "chai";
 import { test, suite } from "mocha";
-// TODO:
-//  -- https://sinonjs.org/
-//  -- also research TDD syntax
-//  -- https://www.chaijs.com/
-//  -- https://sinonjs.org/
-//  -- https://javascript.info/testing-mocha
+import { helloWorld } from "../js/plot.js";
 
 suite("Array", function () {
     suite("#indexOf()", function () {
         test("should return -1 when the value is not present", function () {
             assert.equal([1, 2, 3].indexOf(4), -1);
         });
+    });
+});
+
+suite("Import", function () {
+    test("Hello world", function () {
+        assert.equal(helloWorld(), 1);
     });
 });
